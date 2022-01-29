595121a1c7716517a4378c25b0a3551958841934
Andrey <65595475+AndreyMiruk@users.noreply.github.com>
2022-01-29 16:40:16 +0300

    lesson-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { Message } from './components';
+
+const messageText = 'text';
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Message text={messageText} />
     </div>
   );
-}
+};
 
-export default App;
+
